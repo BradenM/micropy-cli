ab28b3d92ba62f40a953af79455f935a9cfa1de0
Braden Mars <BradenM@users.noreply.github.com>
2019-05-31 02:27:21 -0500

    test: Added Create Script Retrieval Test

--- tests/test_micropy.py
+++ tests/test_micropy.py
@@ -4,6 +4,7 @@ import pytest
 from micropy import micropy
 from pathlib import Path
 import os
+import requests
 
 
 def test_setup(fs_micropy):
@@ -24,3 +25,14 @@ def test_add_stub(fs_micropy, mock_micropy):
     assert stub in mock_micropy.STUBS
     assert stub.path in mock_micropy.STUB_DIR.iterdir()
     assert stub.path.exists()
+
+
+def test_retrieve_create(fs_micropy, mock_micropy, requests_mock):
+    """Tests createstubs.py retrieval"""
+    requests_mock.register_uri(
+        'GET', mock_micropy.CREATE_STUBS_URL, text='CREATE_STUBS_SCRIPT')
+    mock_micropy.retrieve_create_script()
+    assert mock_micropy.CREATE_STUBS.exists()
+    assert mock_micropy.CREATE_STUBS.read_text() == 'CREATE_STUBS_SCRIPT'
+
+
