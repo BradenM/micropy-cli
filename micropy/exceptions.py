c8f362162503de60af7a42b5ccf465e16130ace2
Braden Mars <BradenM@users.noreply.github.com>
2019-06-08 21:52:42 -0500

    feat(stubs): Added Basic Stub Exceptions

---
+++ micropy/exceptions.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""Micropy Exceptions"""
+
+from micropy.logger import Log
+
+
+class StubError(Exception):
+    """Exception for any errors raised by stubs"""
+
+    def __init__(self, stub, message=None):
+        self.stub = stub
+        self.message = message
+        if message is None:
+            message = "An error occured with this stub."
+
+
+class StubValidationError(StubError):
+    """Raised when a stub fails validation"""
+
+    def __init__(self, stub, errors):
+        errs = '\n'.join(errors)
+        msg = f"Stub at [{stub.path}] encountered the following validation errors: {errs}"
+        super().__init__(stub, message=msg)
+
+    def __str__(self):
+        return self.message
