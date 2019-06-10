806ce7c5c5f1e4d2b41fd579447aa1247b5a5f26
Braden Mars <BradenM@users.noreply.github.com>
2019-06-10 02:01:31 -0500

    fix(setup): Fix Missing .vscode Template Files
    They have have been accidentally ignored by git...

--- tests/test_project.py
+++ tests/test_project.py
@@ -23,3 +23,15 @@ def test_project_ctx(mock_micropy, tmp_path):
     assert exp_pylint_0 == proj.context['pylint'].pop(str(test_stub))
     assert len(proj_stubs) == len(proj.context['pylint'].keys())
     assert exp_vscode == proj.context["vscode"]
+
+
+def test_project_structure(mock_micropy, mock_cwd):
+    """Test if project creates files"""
+    proj_stubs = mock_micropy.STUBS[:2]
+    proj = Project("ProjName", proj_stubs)
+    proj.create()
+    templ_files = sorted([i.name for i in (
+        mock_micropy.TEMPLATE / '{{ cookiecutter.project }}').glob("**/*")])
+    proj_files = sorted([i.name for i in proj.path.glob("**/*")])
+    print("Project Files:", proj_files)
+    assert templ_files == proj_files
