8f8e5cf6fb00fd1d8a68786b2d40248f2c206636
Braden Mars <BradenM@users.noreply.github.com>
2019-06-13 17:28:02 -0500

    test(project): Added Pylint Template Test

--- tests/test_template.py
+++ tests/test_template.py
@@ -2,6 +2,7 @@
 
 from micropy.project.template import TemplateProvider
 import json
+import pylint.lint
 
 
 def test_vscode_template(mock_micropy, tmp_path):
@@ -21,6 +22,22 @@ def test_vscode_template(mock_micropy, tmp_path):
     assert sorted(stub_paths) == sorted(
         content["python.autoComplete.extraPaths"])
     assert expected_path.exists()
+
+
+def test_pylint_template(mock_micropy, tmp_path):
+    stubs = mock_micropy.STUBS[:3]
+    prov = TemplateProvider()
+    prov.render_to("pylint", tmp_path, stubs=stubs)
+    expected_path = tmp_path / '.pylintrc'
+    assert expected_path.exists()
+    # Will Pylint load it?
+    try:
+        lint_args = ["--rcfile", str(expected_path.absolute())]
+        pylint.lint.Run(lint_args)
+    except SyntaxError:
+        pytest.fail(str(SyntaxError))
+    except:  # noqa
+        pass
 
 
 def test_generic_template(mock_micropy, tmp_path):
