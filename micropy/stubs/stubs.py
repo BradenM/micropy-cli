9c912e82c224ba7abdf0f3e7f4b54406c3ce6787
Braden Mars <BradenM@users.noreply.github.com>
2019-06-22 02:34:09 -0500

    feat(stubs): Stub Manager Basic Implementation

--- micropy/stubs/stubs.py
+++ micropy/stubs/stubs.py
@@ -10,6 +10,49 @@ from micropy.utils import Validator
 from micropy.exceptions import StubValidationError
 
 
+class StubManager:
+    """Manager for Stub Instances"""
+    _schema = Path(__file__).parent / 'schema.json'
+    _loaded = []
+
+    def __init__(self):
+        self.log = Log.add_logger('Stubs', 'yellow')
+
+    def __iter__(self):
+        return iter(self._loaded)
+
+    def _load(self, path):
+        """Loads a stub"""
+        try:
+            self.validate(path)
+        except StubValidationError:
+            pass
+        else:
+            stub = Stub(path)
+            self.loaded.append(stub)
+            return stub
+
+    def validate(self, path):
+        """Validates stubs"""
+        self.log.debug(f"Validating: {path}")
+        stub_info = path / 'modules.json'
+        val = Validator(self._schema)
+        try:
+            val.validate(stub_info)
+        except FileNotFoundError:
+            raise StubValidationError(
+                path, [f"{path.name} contains no modules.json file!"])
+        except Exception as e:
+            raise StubValidationError(path, str(e))
+
+    def load_from(self, directory):
+        """Load all stubs in a directory"""
+        dir_path = Path(str(directory)).resolve()
+        dirs = dir_path.iterdir()
+        stubs = [self._load(d) for d in dirs]
+        return stubs
+
+
 class Stub:
     """Handles Stub Files
 
@@ -17,12 +60,10 @@ class Stub:
     :param Optional[str] copy_to: directory to copy stub to if it validates
 
     """
-    SCHEMA = Path(__file__).parent / 'schema.json'
 
     def __init__(self, path, copy_to=None, **kwargs):
         self.path = path.absolute()
         self.log = Log.add_logger('Stubs', 'yellow')
-        self.validate(path)
         module = self.path / 'modules.json'
         info = json.load(module.open())
         device = info.pop(0)
@@ -36,19 +77,6 @@ class Stub:
         self.version = device.get('version')
         if copy_to is not None:
             self.copy_to(copy_to)
-
-    def validate(self, path):
-        """Validates stubs"""
-        self.log.debug(f"Validating: {path}")
-        stub_info = path / 'modules.json'
-        val = Validator(self.SCHEMA)
-        try:
-            val.validate(stub_info)
-        except FileNotFoundError:
-            raise StubValidationError(
-                self, [f"{path.name} contains no modules.json file!"])
-        except Exception as e:
-            raise StubValidationError(self, str(e))
 
     def copy_to(self, dest):
         """Copy stub to a directory"""
