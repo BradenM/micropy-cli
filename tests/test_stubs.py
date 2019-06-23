a1f48e0a0cc9c6d56d5ad41f8691f09c8a1e18e9
Braden Mars <BradenM@users.noreply.github.com>
2019-06-22 23:00:55 -0500

    test(stubs): Various Tests new StubManager add method

--- tests/test_stubs.py
+++ tests/test_stubs.py
@@ -28,7 +28,7 @@ def test_bad_stub(tmp_path):
 def test_valid_stub(shared_datadir):
     """should have all attributes"""
     stub_path = shared_datadir / 'esp8266_test_stub'
-    stub = stubs.Stub(stub_path)
+    stub = stubs.stubs.Stub(stub_path)
     expect_module = {
         "file": "/stubs/esp8266_test_stub/micropython.py",
         "module": "micropython"
@@ -39,5 +39,40 @@ def test_valid_stub(shared_datadir):
     assert stub.version == "v1.9.4-8-ga9a3caad0 on 2018-05-11"
     assert stub.machine == "ESP module with ESP8266"
     assert stub.sysname == "esp8266"
+    assert stub.name == "esp8266@v1.9.4-8-ga9a3caad0 on 2018-05-11"
     assert expect_module in stub.modules
     assert str(stub) == "esp8266@v1.9.4-8-ga9a3caad0 on 2018-05-11"
+
+
+def test_add_single_stub(shared_datadir, tmp_path):
+    """should add a single stub"""
+    stub_path = shared_datadir / 'esp8266_test_stub'
+    manager = stubs.StubManager()
+    manager.add(stub_path, dest=tmp_path)
+    assert len(manager) == 1
+    assert stub_path.name in [d.name for d in tmp_path.iterdir()]
+
+
+def test_add_stubs_from_dir(datadir, tmp_path):
+    """should add all valid stubs in directory"""
+    manager = stubs.StubManager()
+    manager.add(datadir, dest=tmp_path)
+    assert len(manager) == 2
+    assert len(list(tmp_path.iterdir())) - 1 == len(manager)
+
+
+def test_add_with_resource(datadir, tmp_path):
+    """should not require dest kwarg"""
+    manager = stubs.StubManager(resource=tmp_path)
+    manager.add(datadir)
+    assert len(manager) == 2
+    # Subtract 1 cause tmp_path has datadir in it for some unrelated reason
+    # as in, before adding stubs
+    assert len(list(tmp_path.iterdir())) - 1 == len(manager)
+
+
+def test_add_no_resource_no_dest(datadir):
+    """should fail with typeerror"""
+    manager = stubs.StubManager()
+    with pytest.raises(TypeError):
+        manager.add(datadir)
