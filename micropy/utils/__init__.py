8588cf79f305c6291b950e6d6015e143b4a6625e
Braden Mars <BradenM@users.noreply.github.com>
2019-06-23 20:33:07 -0500

    feat(pyb): PyboardWrapper Utility
    Wraps rshell/pyboard in a class and provides the basic "run file", "copy dir" functionality that Mic
    ropy-cli needs to create stubs on a device

--- micropy/utils/__init__.py
+++ micropy/utils/__init__.py
@@ -3,5 +3,6 @@
 """MicroPy Utility Module"""
 
 from .validate import Validator
+from .pybwrapper import PyboardWrapper
 
-__all__ = ["Validator"]
+__all__ = ["Validator", "PyboardWrapper"]
---
+++ micropy/utils/pybwrapper.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+import rshell.main as rsh
+from micropy.logger import Log
+from pathlib import Path
+from contextlib import contextmanager
+
+
+class PyboardWrapper:
+    """Wrapper for rshell/pyboard
+
+    Exposes the basic run/copy functionality
+    Micropy needs
+
+    Args:
+        port (str): Port of Pyboard
+
+    Kwargs:
+        connect (bool): Connect on init. Defaults to True
+    """
+
+    def __init__(self, port, connect=True):
+        self.connected = False
+        self.port = port
+        self.rsh = rsh
+        self.rsh.ASCII_XFER = False
+        self.log = Log().add_logger('PyboardWrapper')
+        if connect:
+            return self.connect()
+
+    def _pyb_path(self, path):
+        """returns path relative to pyboard
+
+        Args:
+            path (str): path to resolve
+        """
+        _path = path
+        if path[0] == '/':
+            _path = path[1:]
+        pyb_path = f"{self.pyb_root}{_path}"
+        return pyb_path
+
+    @contextmanager
+    def repl(self):
+        """Pyboard raw repl context manager"""
+        self.pyboard.enter_raw_repl()
+        try:
+            yield self.pyboard
+        finally:
+            self.pyboard.exit_raw_repl()
+
+    def connect(self):
+        """connect to pyboard"""
+        self.log.debug(f"connecting to pydevice @ {self.port}")
+        try:
+            self.rsh.connect(self.port)
+        except SystemExit as e:
+            self.log.debug(f"failed to connect: {str(e)}")
+            raise e
+        else:
+            self.log.debug("connected!")
+            self.connected = True
+
+    @property
+    def pyboard(self):
+        """rshell pyboard instance"""
+        if self.connected:
+            dev = rsh.DEVS[0]
+            return dev.pyb
+
+    @property
+    def pyb_root(self):
+        """pyboard root dirname"""
+        if self.connected:
+            dev = rsh.DEVS[0]
+            return getattr(dev, 'name_path', '/pyboard/')
+
+    def run(self, path):
+        """Execute a script on the pyboard
+
+        Args:
+            path (str): path to file
+        """
+        file_path = Path(str(path)).resolve()
+        with self.repl():
+            out_bytes = self.pyboard.execfile(file_path)
+            out = str(out_bytes, 'utf-8')
+            return out
+
+    def list_dir(self, path):
+        """List directory on pyboard
+
+        Args:
+            path (str): path to directory
+        """
+        dir_path = self._pyb_path(path)
+        tree = self.rsh.auto(rsh.listdir, dir_path)
+        return tree
+
+    def copy_dir(self, path, dest, rsync={}):
+        """Copy directory from pyboard to machine
+
+        Args:
+            path (str): path to directory
+            dest (str): destination to copy to
+            rsync (dict, optional): additonal args to pass to rsync call.
+                Defaults to {}
+        """
+        dir_path = self._pyb_path(path)
+        dest_path = Path(str(dest))
+        rsync_args = {
+            "recursed": True,
+            "mirror": False,
+            "dry_run": False,
+            "print_func": lambda * args: None,
+            "sync_hidden": False
+        }
+        rsync_args.update(rsync)
+        self.rsh.rsync(dir_path, str(dest_path), **rsync_args)
+        out_dir = dest_path / Path(path).name
+        return out_dir
