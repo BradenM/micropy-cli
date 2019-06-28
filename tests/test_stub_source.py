c5646902d8fc7ba39b1e6198bcd89491c9b321a2
Braden Mars <BradenM@users.noreply.github.com>
2019-06-27 23:31:44 -0500

    test(source): Tests for Stub Repo

--- tests/test_stub_source.py
+++ tests/test_stub_source.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 import pytest
 
 from micropy.stubs import source
@@ -49,3 +51,25 @@ def test_source_ready(shared_datadir, test_urls, tmp_path, mocker,
         print(list(source_path.parent.iterdir()))
         assert source_path.exists()
         assert str(source_path) == str(expected_path)
+
+
+def test_repo_resolve(test_urls, shared_datadir, mocker):
+    test_source = shared_datadir / 'test_source.json'
+    mock_get = mocker.patch.object(source.requests, 'get')
+    test_bytes = test_source.open('rb').read()
+    content_val = mocker.PropertyMock(return_value=test_bytes)
+    type(mock_get.return_value).content = content_val
+
+    source_def = json.load(test_source.open())['source']
+    mocker.patch.object(source.utils, "ensure_valid_url",
+                        return_value=source_def['location'])
+
+    get_source_mock = mocker.patch.object(source, "get_source")
+
+    repo = source.StubRepo(**source_def)
+    assert repo.name == "Test Repo"
+    assert repo.location == source_def['location']
+    assert repo.ref == "repo.json"
+    get_source_mock.assert_called_once_with(
+        "https://testsource.com/packages/esp8266-micropython-1.9.4.tar.gz")
+    assert len(repo.packages) == 1
