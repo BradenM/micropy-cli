169ad43c0d144ecdab1133d3c8372ea45e4c8972
Braden Mars <BradenM@users.noreply.github.com>
2019-07-08 03:12:35 -0500

    test(cli): Updated Cli Stubs Tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -47,22 +47,25 @@ def test_cli_init(mocker, mock_prompt, runner):
     assert result.exit_code == 0
 
 
-def test_cli_stubs_add(mocker, shared_datadir, runner):
+def test_cli_stubs_add(mocker, shared_datadir, runner, tmp_path):
     """should add stub"""
     test_stub = shared_datadir / 'esp8266_test_stub'
     test_invalid_stub = shared_datadir / 'esp8266_invalid_stub'
-    mock_stubs = mocker.patch.object(cli.mp, "STUBS")
-    mock_stubs.add.side_effect = [cli.exc.StubValidationError,
-                                  cli.exc.StubNotFound,
-                                  True]
+
     mocker.spy(cli.sys, 'exit')
+    mock_log = mocker.patch.object(cli.mp, "log")
     result = runner.invoke(cli.add, [str(test_invalid_stub.resolve())])
     assert cli.sys.exit.called_with(1)
+    mock_log.error.assert_called_once()
     assert result.exit_code == 1
 
     result = runner.invoke(cli.add, ["not-real-stub"])
     assert cli.sys.exit.called_with(1)
+    assert mock_log.error.call_count == 2
     assert result.exit_code == 1
 
+    res_mock = mocker.patch.object(
+        cli.mp, "STUBS", new_callable=mocker.PropertyMock)
+    type(res_mock).resource = tmp_path
     result = runner.invoke(cli.add, [str(test_stub.resolve())])
     assert result.exit_code == 0
