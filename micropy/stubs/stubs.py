4135055145457a4d13ab2222f15a376924849db8
Braden Mars <BradenM@users.noreply.github.com>
2019-07-08 22:36:00 -0500

    feat(stubs): Device Stubs Firmware Resolution (#25)
    * refactor(stub): Seperate Common Stub Logic to Parent Class
    
    Created 'Stub' parent class to hold common logic between the current DeviceStub and future FirmwareS
    tub classes
    
    * test(stub): Cover NotImplementedError with Stub Child
    
    * test(stubs): Add Tests for FirmwareStubs
    
    * feat(stubs): Firmware Stubs, Manager Resolver
    
    Added FirmwareStub class for representing Firmware specific stubs/modules. Added method to StubManag
    er that resolves the appropriate stub type to use.
    
    * test(stubs): Updated Tests for Firmware Resolving
    
    * fix(stubs): Fix Unreliable Stub Path Resolving
    
    * feat(stubs): Auto Resolve DeviceStub Firmware Modules
    
    When adding a DeviceStub, automatically resolve its Firmware specific modules by determining its Fir
    mwareStub instance. If needed, attempt to retrieve the FirmwareStub from a remote source

--- micropy/stubs/stubs.py
+++ micropy/stubs/stubs.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from shutil import copytree
 
 from micropy import data, utils
-from micropy.exceptions import StubValidationError
+from micropy.exceptions import StubError, StubValidationError
 from micropy.logger import Log
 from micropy.stubs import source
 
@@ -15,23 +15,26 @@ class StubManager:
 
     Kwargs:
         resource (str): Default resource path
+        repos ([StubRepo]): Repos for Remote Stubs
 
     Raises:
-        StubValidationError: a stub is missing a def file
+        StubError: a stub is missing a def file
         StubValidationError: a stubs def file is not valid
 
     Returns:
         object: Instance of StubManager
     """
     _schema = data.SCHEMAS / 'stubs.json'
+    _firm_schema = data.SCHEMAS / 'firmware.json'
 
     def __init__(self, resource=None, repos=None):
         self._loaded = set()
+        self._firmware = set()
         self.resource = resource
         self.repos = repos
         self.log = Log.add_logger('Stubs', 'yellow')
         if self.resource:
-            self.load_from(resource)
+            self.load_from(resource, strict=False)
 
     def __iter__(self):
         return iter(self._loaded)
@@ -39,32 +42,110 @@ class StubManager:
     def __len__(self):
         return len(self._loaded)
 
-    def _load(self, stub_source, *args, **kwargs):
-        """Loads a stub"""
+    def _load(self, stub_source, strict=True, *args, **kwargs):
+        """Loads a stub into StubManager
+
+        Args:
+            stub_source (StubSource): Stub Source Instance
+            strict (bool, optional): Raise Exception if stub fails to resolve.
+                Defaults to True.
+
+        Raises:
+            e: Exception raised by resolving failure
+
+        Returns:
+            Stub: Instance of Stub
+        """
         with stub_source.ready() as src_path:
             try:
-                self.validate(src_path)
-            except StubValidationError as e:
+                stub_type = self.resolve_stub(src_path)
+            except Exception as e:
                 self.log.debug(f"{src_path.name} failed to validate: {e}")
+                if strict:
+                    raise e
             else:
-                stub = Stub(src_path, *args, **kwargs)
+                if stub_type is FirmwareStub:
+                    fware = stub_type(src_path, *args, **kwargs)
+                    self._firmware.add(fware)
+                    self.log.debug(f"Firmware Loaded: {fware}")
+                    return fware
+                stub = stub_type(src_path, *args, **kwargs)
+                fware = self.resolve_firmware(stub)
+                if fware:
+                    stub.firmware = fware
                 self._loaded.add(stub)
                 self.log.debug(f"Loaded: {stub}")
                 return stub
 
-    def validate(self, path):
-        """Validates stubs"""
+    def resolve_firmware(self, stub):
+        """Resolves FirmwareStub for DeviceStub instance
+
+        Args:
+            stub (DeviceStub): Stub to resolve
+
+        Returns:
+            FirmwareStub: Instance of FirmwareStub
+            NoneType: None if an appropriate
+                FirmwareStub cannot be found
+        """
+        fware_name = stub.firmware_name
+        results = (f for f in self._firmware if f.firmware == fware_name)
+        fware = next(results, None)
+        if not fware:
+            try:
+                fware = self.add(fware_name)
+            except Exception:
+                return None
+            else:
+                return fware
+        return fware
+
+    def validate(self, path, schema=None):
+        """Validates given stub path against its schema
+
+        Args:
+            path (str): path to validate
+            schema (str, optional): Path to schema. Defaults to None.
+                If None, the DeviceStub schema is used.
+
+        Raises:
+            StubError: Raised if no info file can be found
+            StubValidationError: Raised if the info file fails validation
+        """
         self.log.debug(f"Validating: {path}")
+        schema = schema or self._schema
         path = Path(path).resolve()
         stub_info = path / 'info.json'
-        val = utils.Validator(self._schema)
+        val = utils.Validator(schema)
         try:
             val.validate(stub_info)
         except FileNotFoundError:
-            raise StubValidationError(
-                path, [f"{path.name} contains no info file!"])
+            raise StubError(f"{path.name} contains no info file!")
         except Exception as e:
             raise StubValidationError(path, str(e))
+
+    def resolve_stub(self, path):
+        """Resolves appropriate stub type
+
+        Args:
+            path (str): path to stub
+
+        Returns:
+            cls: Appropriate class for stub
+        """
+        try:
+            self.validate(path)
+        except StubValidationError:
+            try:
+                self.validate(path, schema=self._firm_schema)
+            except Exception as e:
+                raise e
+            else:
+                return FirmwareStub
+        except Exception as e:
+            raise e
+        else:
+            return DeviceStub
 
     def is_valid(self, path):
         """Check if stub is valid without raising an exception
@@ -76,19 +157,48 @@ class StubManager:
             bool: True if stub is valid
         """
         try:
-            self.validate(path)
-        except StubValidationError:
+            self.resolve_stub(path)
+        except Exception:
             return False
         else:
             return True
 
     def load_from(self, directory, *args, **kwargs):
-        """Load all stubs in a directory"""
+        """Recursively loads stubs from a directory
+
+        Args:
+            directory (str): Path to load from
+
+        Returns:
+            [DeviceStub]: List of loaded Stubs
+        """
         dir_path = Path(str(directory)).resolve()
         dirs = dir_path.iterdir()
         stubs = [self._load(source.get_source(d), *args, **kwargs)
                  for d in dirs]
         return stubs
+
+    def _should_recurse(self, location):
+        """Checks for multiple stubs in a location
+
+        Args:
+            location (str): location of potential stub
+
+        Raises:
+            StubError: No info files could be found
+
+        Returns:
+            bool: True if multiple stubs are found
+        """
+        if not Path(location).exists():
+            return False
+        path = Path(location).resolve()
+        info_glob = list(path.rglob("info.json"))
+        if len(info_glob) == 0:
+            raise StubError(f"{path.name} contains no info file!")
+        if len(info_glob) > 1:
+            return True
+        return False
 
     def add(self, location, dest=None):
         """Add stub(s) from source
@@ -105,35 +215,29 @@ class StubManager:
         if not _dest:
             raise TypeError("No Stub Destination Provided!")
         dest = Path(str(_dest)).resolve()
-        if utils.is_existing_dir(location) and not self.is_valid(location):
-            return self.load_from(location, copy_to=dest)
+        if self._should_recurse(location):
+            return self.load_from(location, strict=False, copy_to=dest)
         stub_source = source.get_source(location)
         return self._load(stub_source, copy_to=dest)
 
 
 class Stub:
-    """Handles Stub Files
+    """Abstract Parent for Stub Related Classes
 
-    :param str path: path to stub
-    :param Optional[str] copy_to: directory to copy stub to if it validates
+    Not Meant to be instantiated directly. Contains common logic
+    between different types of Stubs (ex. Firmware vs Device)
 
+    Raises:
+        NotImplementedError: name property is not implemented
+
+    Returns:
+        Instance of Stub
     """
 
     def __init__(self, path, copy_to=None, **kwargs):
-        self.path = path.absolute()
-        self.log = Log.add_logger('Stubs', 'yellow')
+        self.path = Path(path).resolve()
         ref = self.path / 'info.json'
-        info = json.load(ref.open())
-
-        self.stubs = self.path / 'stubs'
-        self.frozen = self.path / 'frozen'
-
-        self.firmware = info.get("firmware")
-        self.sysname = self.firmware.get('sysname')
-        self.version = self.firmware.get('version')
-
-        self.name = (
-            f"{self.sysname}-{self.firmware_name}-{self.version}")
+        self.info = json.load(ref.open())
         if copy_to is not None:
             self.copy_to(copy_to)
 
@@ -146,16 +250,9 @@ class Stub:
         return self
 
     @property
-    def firmware_name(self):
-        """Return an appropriate firmware name
-
-        Returns:
-            str: Name of Firmware
-        """
-        fware = self.firmware.get('name', None)
-        if not fware:
-            fware = self.firmware.get('firmware')
-        return fware
+    def name(self):
+        """Human friendly stub name"""
+        raise NotImplementedError
 
     def __eq__(self, other):
         return self.name == getattr(other, 'name', None)
@@ -163,9 +260,76 @@ class Stub:
     def __hash__(self):
         return hash(self.name)
 
-    def __repr__(self):
-        return (f"Stub(sysname={self.sysname}, firmware={self.firmware_name},"
-                f" version={self.version}, path={self.path})")
-
     def __str__(self):
         return self.name
+
+
+class DeviceStub(Stub):
+    """Handles Device Specific Stubs
+
+    Args:
+        path (str): path to stub
+        copy_to (str, optional): Path to copy Stub to. Defaults to None.
+
+    Returns:
+        Device Stub Instance
+    """
+
+    def __init__(self, path, copy_to=None, **kwargs):
+        super().__init__(path, copy_to, **kwargs)
+
+        self.stubs = self.path / 'stubs'
+        self.frozen = self.path / 'frozen'
+
+        self.firmware = self.info.get("firmware")
+        self.sysname = self.firmware.get('sysname')
+        self.version = self.firmware.get('version')
+
+    @property
+    def firmware_name(self):
+        """Return an appropriate firmware name
+
+        Returns:
+            str: Name of Firmware
+        """
+        if isinstance(self.firmware, FirmwareStub):
+            return self.firmware.firmware
+        fware = self.firmware.get('name', None)
+        if not fware:
+            fware = self.firmware.get('firmware')
+        return fware
+
+    @property
+    def name(self):
+        return f"{self.sysname}-{self.firmware_name}-{self.version}"
+
+    def __repr__(self):
+        return (f"DeviceStub(sysname={self.sysname}, firmware="
+                f"{self.firmware_name}, version={self.version}, "
+                f"path={self.path})")
+
+
+class FirmwareStub(Stub):
+    """Handles Firmware Specific Modules
+
+    Args:
+        path (str): path to stub
+        copy_to (str, optional): Path to copy Stub to. Defaults to None.
+
+    Returns:
+        FirmwareStub Instance
+    """
+
+    def __init__(self, path, copy_to=None, **kwargs):
+        super().__init__(path, copy_to=copy_to, **kwargs)
+
+        self.frozen = self.path / 'frozen'
+        self.repo = self.info.get('repo')
+        self.firmware = self.info.get('firmware')
+
+    @property
+    def name(self):
+        return self.info.get('name')
+
+    def __repr__(self):
+        return f"FirmwareStub(firmware={self.firmware}, repo={self.repo})"
