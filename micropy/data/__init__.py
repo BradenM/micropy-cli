a0df27be8d075c5660f2ce69d9b949b77f488ce2
Braden Mars <bradenmars@bradenmars.me>
2019-10-05 01:57:33 -0500

    refactor(data): Move all Data Paths to Data Module

--- micropy/data/__init__.py
+++ micropy/data/__init__.py
@@ -10,8 +10,32 @@ data files used by MicropyCli
 
 from pathlib import Path
 
+__all__ = [
+    "ROOT",
+    "SCHEMAS",
+    "REPO_SOURCES",
+    "FILES",
+    "STUB_DIR",
+    "LOG_FILE",
+    "STUBBER"
+]
+
+# Paths
 MOD_PATH = Path(__file__).parent
-PATH = MOD_PATH.resolve()
+PATH = MOD_PATH.absolute()
+ROOT = MOD_PATH.parent.absolute()
+
+# Stub Schemas
 SCHEMAS = PATH / 'schemas'
 
-__all__ = ["PATH", "SCHEMAS"]
+# Default Stub Sources
+REPO_SOURCES = PATH / 'sources.json'
+
+# Application Data
+FILES = Path.home() / '.micropy'
+STUB_DIR = FILES / 'stubs'
+LOG_FILE = FILES / 'micropy.log'
+
+# Libraries
+LIB = ROOT / 'lib'
+STUBBER = LIB / 'stubber'
