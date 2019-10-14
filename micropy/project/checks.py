BradenM/micropy-cli

commit 06b894ffdcf1eb064fd69e527d9ad592a08ef0d8

fix: Project Fails to Init due to Checks on Windows
The VSCode template check causes a FileNotFound error when executed in a Windows environment. This w
as due to the subprocess spawned not being executed in a shell.

Fixes #59

micropy/project/checks.py
@@ -21,7 +21,8 @@ def iter_vscode_ext(name=None):
         name (str, optional): Name of Extension to Yield
     """
     _cmd = "code --list-extensions --show-versions"
-    proc = subproc.run(_cmd.split(), text=True, capture_output=True)
+    proc = subproc.run(_cmd.split(), text=True,
+                       capture_output=True, shell=True)
     results = [e.strip() for e in proc.stdout.splitlines()]
     for ext in results:
         ename, vers = ext.split('@')

commit 15974b15d00196869454ab67b2f435a1b2364b13

fix: VSCode Template Check always Fails on Linux
Broken by 06b894f - Windows still works fine

micropy/project/checks.py
@@ -21,7 +21,7 @@ def iter_vscode_ext(name=None):
         name (str, optional): Name of Extension to Yield
     """
     _cmd = "code --list-extensions --show-versions"
-    proc = subproc.run(_cmd.split(), text=True,
+    proc = subproc.run(_cmd, text=True,
                        capture_output=True, shell=True)
     results = [e.strip() for e in proc.stdout.splitlines()]
     for ext in results: