88f30cc37f97bd424829fbd2d91025e88f5e5bae
Braden Mars <bradenmars@bradenmars.me>
2019-11-29 20:36:28 -0600

    feat(config): New Interface with file/memory autosync and dot notation
    Adds micropy.config.Config class which acts as an interface to any generic configuration needed. Can
    be accessed via dot notation (via string) and automatically syncs the config file and config in mem
    ory.

---
+++ micropy/config/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Configuration files and interfaces for them"""
+
+from .config import Config
+from .config_json import JSONConfigSource
+
+__all__ = ["Config", "JSONConfigSource"]
