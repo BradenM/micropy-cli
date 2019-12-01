df857926b79ab48aba10b96f01c40118c2b7a2be
Braden Mars <bradenmars@bradenmars.me>
2019-11-30 23:29:49 -0600

    style(packages): Add docstrings, Code Cleanup

--- micropy/packages/source_package.py
+++ micropy/packages/source_package.py
@@ -12,6 +12,14 @@ from .source import DependencySource
 
 
 class PackageDependencySource(DependencySource):
+    """Dependency Source for pypi packages.
+
+    Args:
+        package (Package): Package source points too.
+        format_desc (Optional[Callable[..., Any]], optional): Callback to format progress bar description.
+            Defaults to None.
+
+    """
 
     def __init__(self, package: Package,
                  format_desc: Optional[Callable[..., Any]] = None):
@@ -28,12 +36,33 @@ class PackageDependencySource(DependencySource):
         return utils.get_url_filename(self.source_url)
 
     def fetch(self) -> bytes:
+        """Fetch package contents into memory.
+
+        Returns:
+            bytes: Package archive contents.
+
+        """
         self.log.debug(f"fetching package: {self.file_name}")
         desc = self.format_desc(self.file_name)
         content = utils.stream_download(self.source_url, desc=desc)
         return content
 
     def __enter__(self) -> Union[Path, List[Tuple[Path, Path]]]:
+        """Prepare Pypi package for installation.
+
+        Extracts the package into a temporary directory then
+        generates stubs for type hinting.
+        This helps with intellisense.
+
+        If the dependency is a module, a list
+        of tuples with the file and stub path, respectively,
+        will be returned. Otherwise, the path to the package
+        root will be returned.
+
+        Returns:
+            Union[Path, List[Tuple[Path, Path]]]: Root package path or list of files.
+
+        """
         with TemporaryDirectory() as tmp_dir:
             with self.handle_cleanup():
                 tmp = Path(tmp_dir)
