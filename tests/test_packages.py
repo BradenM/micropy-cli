9461710695a11b0bf0e8bddc0d31942a5853eaab
Braden Mars <bradenmars@bradenmars.me>
2019-11-30 23:16:55 -0600

    test(deps): Add Tests for Package module

---
+++ tests/test_packages.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+import pytest
+
+from micropy import packages, utils
+
+
+class TestPackages:
+
+    class MockSource:
+        def __init__(self, pkg, has_init):
+            self.pkg = pkg
+            self.has_init = has_init
+
+    @pytest.fixture(params=[True, False], ids=['package', 'module'])
+    def mock_source(self, request, mock_pkg):
+        if request.param:
+            # true packages vs file
+            (mock_pkg / '__init__.py').touch()
+        return self.MockSource(mock_pkg, request.param)
+
+    @pytest.fixture(params=[True, False, None], ids=['package', 'module', 'dir'])
+    def mock_source_path(self, request, tmp_path):
+        path = (tmp_path / 'file.py')
+        pkg = path
+        if request.param is True:
+            pkg = tmp_path
+            path = (tmp_path / '__init__.py')
+        if request.param is None:
+            pkg = tmp_path
+        path.touch()
+        return self.MockSource(pkg, request.param)
+
+    @pytest.mark.parametrize(
+        'package,expect',
+        [
+            ('picoweb', packages.PackageDependencySource),
+            ('-e /foobar/pkg', packages.LocalDependencySource)
+        ]
+    )
+    def test_factory(self, package, expect):
+        source = packages.create_dependency_source(package)
+        assert isinstance(source, expect)
+
+    @pytest.mark.parametrize(
+        'requirement,expect',
+        [
+            (['picoweb'], ['picoweb', '*']),
+            (['picoweb==^7.1'], ['picoweb', '==^7.1']),
+            (['BlynkLib==0.0.0'], ['blynklib', '==0.0.0']),
+            (['-e /foobar/somepkg', 'somepackage'], ['somepackage', '*']),
+            (['-e /foobar/somepkg'], ['somepkg', '*']),
+
+        ]
+    )
+    def test_package(self, mock_pkg, requirement, expect):
+        source = packages.create_dependency_source(*requirement)
+        pkg = source.package
+        assert pkg.name == expect[0]  # name
+        assert pkg.pretty_specs == expect[1]  # specs
+
+    def test_package_source(self, mock_source):
+        def format_desc(x): return f"desc{x}"
+        source = packages.create_dependency_source('blynklib', format_desc=format_desc)
+        with source as files:
+            if mock_source.has_init:
+                assert isinstance(files, Path)
+                assert mock_source.pkg.name == files.name
+            else:
+                assert isinstance(files, list)
+                file_one = files[0]
+                assert file_one[0].name == 'file.py'
+                assert file_one[1].name == 'file.pyi'
+
+    def test_package_path(self, mock_source_path):
+        source = packages.create_dependency_source(f'-e {mock_source_path.pkg}')
+        with source as files:
+            if mock_source_path.has_init:
+                # is proper package
+                assert files.is_dir()
+            if mock_source_path.has_init is False:
+                # is module
+                assert files.is_file()
+            if mock_source_path.has_init is None:
+                # is just a dir
+                assert files.is_dir()
+                assert files == source._path
