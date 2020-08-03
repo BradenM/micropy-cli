e590ea6c1806d0405da739e23fea7ad8a534d15e
Braden Mars <bradenmars@bradenmars.me>
2020-08-03 00:49:44 -0500

    feat(package): add VCSDependencySource class for supporting VCS requirements

--- micropy/packages/source_package.py
+++ micropy/packages/source_package.py
@@ -5,6 +5,7 @@ import shutil
 from pathlib import Path
 from tempfile import mkdtemp
 from typing import Any, Callable, List, Optional, Tuple, Union
+from git import Repo
 
 
 from micropy import utils
@@ -84,6 +85,70 @@ class PackageDependencySource(DependencySource):
         self.tmp_path = Path(mkdtemp())
         with self.handle_cleanup():
             path = utils.extract_tarbytes(self.fetch(), self.tmp_path)
+            stubs = self.generate_stubs(path)
+            pkg_root = self.get_root(path)
+        return pkg_root or stubs
+
+    def __exit__(self, *args):
+        shutil.rmtree(self.tmp_path, ignore_errors=True)
+        return super().__exit__(*args)
+
+
+class VCSDependencySource(DependencySource):
+    """Dependency Source for vcs packages."""
+
+    def __init__(self, package: Package, format_desc: Optional[Callable[..., Any]] = None):
+        super().__init__(package)
+        self.log.debug(
+            f'VCS package!, {self.package.revision}@{self.package.vcs}@{self.package.full_name}')
+        self._repo: Optional[Repo] = None
+        try:
+            utils.ensure_valid_url(self.repo_url)
+        except Exception:
+            raise RequirementNotFound(
+                f"{self.repo_url} is not a valid VCS url!", package=self.package)
+        else:
+            self.format_desc = format_desc or (lambda n: n)
+
+    @property
+    def repo_url(self) -> str:
+        return self.package.uri
+
+    @property
+    def source_url(self) -> str:
+        return self.package.uri
+
+    @property
+    def file_name(self) -> str:
+        return self.package.name
+
+    def fetch(self, dest_path: Path) -> Path:
+        """Clones VCS repository to a given directory.
+
+        Args:
+            dest_path: Path to clone directory too.
+
+        Returns:
+            Path to clone repository.
+
+        """
+        self.log.debug(f"fetching vcs package: ${self.file_name} @ ${self.repo_url}")
+        self.format_desc(self.file_name)
+        self._repo = Repo.clone_from(self.repo_url, str(dest_path))
+        return dest_path
+
+    def __enter__(self) -> Union[Path, List[Tuple[Path, Path]]]:
+        """Prepare VCS repository for installation.
+        
+        See PackageDependencySource.__enter__
+        
+        Returns:
+            Root package path or list of files.
+
+        """
+        self.tmp_path = Path(mkdtemp())
+        with self.handle_cleanup():
+            path = self.fetch(self.tmp_path)
             stubs = self.generate_stubs(path)
             pkg_root = self.get_root(path)
         return pkg_root or stubs
