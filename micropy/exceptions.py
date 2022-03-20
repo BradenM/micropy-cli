6bb9f655494424cb4d9956c1a33ab848126e9ed2
Braden Mars <bradenmars@bradenmars.me>
2022-03-20 06:15:28 -0500

    feat(exc): add PyDeviceError, PyDeviceConnectionError exceptions

--- micropy/exceptions.py
+++ micropy/exceptions.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 """Micropy Exceptions."""
 
 
@@ -15,7 +13,7 @@ class StubError(MicropyException):
         self.stub = stub
         self.message = message
         if message is None:
-            message = "An error occured with this stub."
+            message = "An error occurred with this stub."
 
 
 class StubValidationError(StubError):
@@ -35,7 +33,7 @@ class StubNotFound(StubError):
     def __init__(self, stub_name=None):
         stub_name = stub_name or "Unknown"
         msg = f"{stub_name} is not available!"
-        return super().__init__(msg)
+        super().__init__(msg)
 
 
 class RequirementException(MicropyException):
@@ -48,3 +46,18 @@ class RequirementException(MicropyException):
 
 class RequirementNotFound(RequirementException):
     """A requirement could not be found."""
+
+
+class PyDeviceError(MicropyException):
+    """Generic PyDevice exception."""
+
+    def __init__(self, message: str = None):
+        super().__init__(message)
+        self.message = message
+
+
+class PyDeviceConnectionError(PyDeviceError):
+    _default_message = "Failed to connect to pydevice @ {location}."
+
+    def __init__(self, location: str):
+        super().__init__(self._default_message.format(location=location))
