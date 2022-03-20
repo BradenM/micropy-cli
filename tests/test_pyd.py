de7fc4c87f743a46f2ff2acfe2cda66ef22c67a0
Braden Mars <bradenmars@bradenmars.me>
2022-03-20 06:16:50 -0500

    test(pyd): setup and add basic init tests for pyd backends

---
+++ tests/test_pyd.py
@@ -0,0 +1,102 @@
+from __future__ import annotations
+
+from typing import Literal, Type
+from unittest.mock import MagicMock
+
+import pytest
+from micropy.exceptions import PyDeviceError
+from micropy.pyb import backend_rshell, backend_upydevice
+from micropy.pyb.abc import PyDevice
+from pytest_mock import MockFixture
+
+
+@pytest.fixture
+def mock_upy(mocker: MockFixture):
+    mock_upy = mocker.patch.object(backend_upydevice, "upydevice", autospec=True)
+    return mock_upy
+
+
+@pytest.fixture
+def mock_upy_uos(mocker: MockFixture):
+    mock_uos = mocker.patch.object(backend_upydevice, "UOS", autospec=True)
+    return mock_uos
+
+
+@pytest.fixture
+def mock_rsh(mocker: MockFixture):
+    mock_rsh = mocker.patch.object(backend_rshell, "rsh", autospec=True)
+    return mock_rsh
+
+
+class MockAdapter:
+    backend: Literal["upy", "rsh"]
+    mock: MagicMock
+    mock_uos: MagicMock
+
+    def __init__(self, backend: Literal["upy", "rsh"], mock: MagicMock, mock_uos=None):
+        self.backend = backend
+        self.mock = mock
+        if mock_uos:
+            self.mock_uos = mock_uos
+
+    @property
+    def is_rsh(self) -> bool:
+        return self.backend == "rsh"
+
+    @property
+    def is_upy(self) -> bool:
+        return self.backend == "upy"
+
+    @property
+    def connect(self) -> MagicMock:
+        return self.mock.Device.return_value.connect if self.is_upy else self.mock.connect
+
+
+MOCK_PORT = "/dev/port"
+
+
+@pytest.fixture(params=["upy", "rsh"], scope="class")
+def pymock_setup(request: pytest.FixtureRequest) -> MockAdapter:
+    request.cls.backend = request.param
+    request.cls.pyd_cls = (
+        backend_upydevice.UPYPyDevice
+        if request.cls.backend == "upy"
+        else backend_rshell.RShellPyDevice
+    )
+    _cls = (
+        backend_upydevice.UPYPyDevice if request.param == "upy" else backend_rshell.RShellPyDevice
+    )
+    _consumer = (
+        backend_upydevice.UPYConsumer if request.param == "upy" else backend_rshell.RShellConsumer
+    )
+    yield
+
+
+@pytest.fixture
+def pymock(request: MockFixture, mock_upy_uos):
+    mod_mock = request.getfixturevalue(f"mock_{request.cls.backend}")
+    return MockAdapter(request.cls.backend, mod_mock, mock_upy_uos)
+
+
+@pytest.mark.usefixtures("pymock_setup")
+class TestPyDevice:
+    backend: Literal["upy", "rsh"]
+    pyd_cls: Type[PyDevice]
+
+    def test_init(self, pymock):
+        m = pymock
+        pyd = self.pyd_cls(MOCK_PORT)
+        if self.backend == "upy":
+            m.mock.Device.assert_called_once_with(MOCK_PORT, init=True, autodetect=True)
+        else:
+            assert m.mock.ASCII_XFER is False
+            assert m.mock.QUIET is True
+        m.connect.assert_called_once()
+        assert pyd.connected
+        assert pyd.location == MOCK_PORT
+
+    def test_init__connect_fail(self, pymock):
+        m = pymock
+        m.connect.side_effect = [SystemExit, SystemExit]
+        with pytest.raises(PyDeviceError):
+            self.pyd_cls(MOCK_PORT)
