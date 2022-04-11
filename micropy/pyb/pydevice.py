a2c6b0cf29c8defad765f4a1e3e7455d014304f0
Braden Mars <bradenmars@bradenmars.me>
2022-04-10 23:22:03 -0500

    feat(pyd): use/pass consumer handlers via delegate, expose connect/dc

--- micropy/pyb/pydevice.py
+++ micropy/pyb/pydevice.py
@@ -12,11 +12,12 @@ from .abc import (
     StreamConsumer,
 )
 from .backend_upydevice import UPyDeviceBackend
+from .consumers import ConsumerDelegate
 
 
 class PyDevice(MetaPyDevice):
-    stream_consumer: StreamConsumer
-    message_consumer: MessageConsumer
+    pydevice: MetaPyDeviceBackend
+    consumer: ConsumerDelegate
 
     def __init__(
         self,
@@ -28,27 +29,30 @@ class PyDevice(MetaPyDevice):
         message_consumer: MessageConsumer = None,
     ):
         self.pydevice = backend().establish(location)
-        self.stream_consumer = stream_consumer
-        self.message_consumer = message_consumer
-        if auto_connect:
+        self.consumer = ConsumerDelegate(stream_consumer, message_consumer)
+        if auto_connect and self.pydevice:
             self.pydevice.connect()
 
     def copy_from(self, source_path: DevicePath, target_path: HostPath) -> None:
         src_path = Path(str(source_path))
-        if src_path.is_dir():
-            return self.pydevice.copy_dir(source_path, target_path)
-        return self.pydevice.pull_file(source_path, target_path)
+        # 'is_dir/file' only works on existing paths.
+        if not src_path.suffix:
+            return self.pydevice.copy_dir(
+                DevicePath(source_path), target_path, consumer=self.consumer
+            )
+        return self.pydevice.pull_file(DevicePath(source_path), target_path, consumer=self.consumer)
 
     def copy_to(self, source_path: HostPath, target_path: DevicePath) -> None:
         src_path = Path(str(source_path))
-        if src_path.is_dir():
+        if not src_path.suffix:
             raise RuntimeError("Copying dirs to device is not yet supported!")
-        return self.pydevice.push_file(source_path, target_path)
+        return self.pydevice.push_file(source_path, target_path, consumer=self.consumer)
+
+    def connect(self):
+        return self.pydevice.connect()
+
+    def disconnect(self):
+        return self.pydevice.disconnect()
 
     def run_script(self, content: AnyStr, target_path: DevicePath | None = None):
-        return self.pydevice.eval_script(
-            contents,
-            target_path,
-            stream_consumer=self.stream_consumer,
-            message_consumer=self.message_consumer,
-        )
+        return self.pydevice.eval_script(content, target_path, consumer=self.consumer)
