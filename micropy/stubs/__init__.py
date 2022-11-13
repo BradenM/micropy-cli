781f7cddcf3912c8da89e790b717b34f04366737
Braden Mars <bradenmars@bradenmars.me>
2022-11-13 04:09:51 -0600

    feat(stubs): add StubRepository for managing stub manifests.

--- micropy/stubs/__init__.py
+++ micropy/stubs/__init__.py
@@ -7,6 +7,23 @@ to stub files/frozen modules and their usage in MicropyCli
 """
 
 from . import source
+from .manifest import StubsManifest
+from .package import StubPackage
+from .repo import StubRepository
+from .repo_package import StubRepositoryPackage
+from .repositories import MicropyStubPackage, MicropythonStubsManifest, MicropythonStubsPackage
+from .repository_info import RepositoryInfo
 from .stubs import StubManager
 
-__all__ = ["StubManager", "source"]
+__all__ = [
+    "StubManager",
+    "source",
+    "StubsManifest",
+    "StubPackage",
+    "StubRepository",
+    "MicropyStubPackage",
+    "MicropythonStubsPackage",
+    "MicropythonStubsManifest",
+    "RepositoryInfo",
+    "StubRepositoryPackage",
+]
