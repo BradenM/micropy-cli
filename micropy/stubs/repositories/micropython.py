f8a40cec9be827e9dd155e41b863cc04f646408f
Braden Mars <bradenmars@bradenmars.me>
2022-11-13 04:09:13 -0600

    feat(stubs): add Micropython stubs package/manifest models.

---
+++ micropy/stubs/repositories/micropython.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from pydantic import Field, validator
+from typing_extensions import Annotated
+
+from ..manifest import StubsManifest
+from ..package import StubPackage
+
+
+class MicropythonStubsPackage(StubPackage):
+    name: str
+    version: Annotated[str, Field(alias="pkg_version")]
+
+    @property
+    def package_name(self) -> str:
+        return f"{self.name}@{self.version}"
+
+
+class MicropythonStubsManifest(StubsManifest):
+    packages: list[MicropythonStubsPackage]
+
+    @validator("packages", pre=True)
+    def _get_packages(cls, v: dict[str, dict]):
+        data = v["data"].values()
+        return list(data)
