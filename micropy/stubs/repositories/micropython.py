65f210297212069492ef6f7bde64a37667dd6524
Braden Mars <bradenmars@bradenmars.me>
2022-11-28 04:13:01 -0600

    feat(stubs): impl `resolve_package_url` for micropython-stubs repo.

--- micropy/stubs/repositories/micropython.py
+++ micropy/stubs/repositories/micropython.py
@@ -1,19 +1,21 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+from distlib.locators import locate
 from pydantic import Field, validator
 from typing_extensions import Annotated
 
 from ..manifest import StubsManifest
 from ..package import StubPackage
 
+if TYPE_CHECKING:
+    from distlib.database import Distribution
+
 
 class MicropythonStubsPackage(StubPackage):
     name: str
     version: Annotated[str, Field(alias="pkg_version")]
-
-    @property
-    def package_name(self) -> str:
-        return f"{self.name}@{self.version}"
 
 
 class MicropythonStubsManifest(StubsManifest):
@@ -26,4 +28,6 @@ class MicropythonStubsManifest(StubsManifest):
         return list(data)
 
     def resolve_package_url(self, package: StubPackage) -> str:
-        pass
+        dist: Distribution = locate(f"{package.name} ({package.version})")
+        dist_url = next(i for i in dist.download_urls if "tar.gz" in i)
+        return dist_url
