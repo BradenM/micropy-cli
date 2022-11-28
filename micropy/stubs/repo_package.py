e257aa56d14a98e3b81ef26c5e8fc80615036d93
Braden Mars <bradenmars@bradenmars.me>
2022-11-28 04:15:05 -0600

    feat(stubs): expose `repo_name`,`versioned_name`,`absolute_versioned_name` on `StubRepositoryPackage`

--- micropy/stubs/repo_package.py
+++ micropy/stubs/repo_package.py
@@ -13,6 +13,10 @@ class StubRepositoryPackage(BaseModel):
         return self.manifest.resolve_package_url(self.package)
 
     @property
+    def repo_name(self) -> str:
+        return self.manifest.repository.name
+
+    @property
     def name(self) -> str:
         return self.package.name
 
@@ -23,3 +27,14 @@ class StubRepositoryPackage(BaseModel):
     @property
     def absolute_name(self) -> str:
         return self.manifest.resolve_package_absolute_name(self.package)
+
+    @property
+    def versioned_name(self) -> str:
+        return self.manifest.resolve_package_versioned_name(self.package)
+
+    @property
+    def absolute_versioned_name(self) -> str:
+        return self.manifest.resolve_package_absolute_versioned_name(self.package)
+
+    def match_exact(self, in_name: str) -> bool:
+        return in_name in [self.absolute_name, self.versioned_name, self.absolute_versioned_name]
