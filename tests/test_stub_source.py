f1742da9d9386e929612f13bb66076a5e62af19d
Braden Mars <bradenmars@bradenmars.me>
2022-12-04 05:00:31 -0600

    test(stubs): remove old `StubRepo` tests.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- tests/test_stub_source.py
+++ tests/test_stub_source.py
@@ -1,7 +1,3 @@
-import json
-
-import pytest
-from micropy.exceptions import StubError
 from micropy.stubs import source
 
 
@@ -38,35 +34,3 @@ def test_source_ready(shared_datadir, test_urls, tmp_path, mocker, test_archive,
         print(list(source_path.parent.iterdir()))
         assert (source_path / "info.json").exists()
         assert len(list(source_path.iterdir())) == 3
-
-
-def test_repo_from_json(shared_datadir, mocker):
-    mocker.patch.object(source.utils, "ensure_valid_url", return_value="https://testsource.com")
-    test_sources = shared_datadir / "test_sources.json"
-    test_repo = json.loads((shared_datadir / "test_repo.json").read_text())
-    mock_get = mocker.patch.object(source.requests, "get")
-    mock_get.return_value.json.return_value = test_repo
-    content = test_sources.read_text()
-    repos = list(source.StubRepo.from_json(content))
-    assert repos[0].name == "Test Repo"
-    assert len(repos) == 1
-
-
-def test_repo_resolve_pkg(mocker, test_urls):
-    url = test_urls["valid"]
-    mocker.patch.object(source.utils, "ensure_valid_url", return_value=url)
-    mocker.patch.object(source.utils, "is_downloadable", return_value=False)
-    source.StubRepo("TestRepo", url, "packages")
-    with pytest.raises(StubError):
-        source.StubRepo.resolve_package("not-valid")
-
-
-def test_repo_search(mocker, test_urls, test_repo):
-    url = test_urls["valid"]
-    mocker.patch.object(source.utils, "ensure_valid_url", return_value=url)
-    results = test_repo.search("esp32-micropython")
-    assert len(results) == 1
-    assert "esp32-micropython-1.11.0" in results
-    results = test_repo.search("esp32")
-    assert len(results) == 2
-    assert sorted(results) == sorted(["esp32-micropython-1.11.0", "esp32_LoBo-esp32_LoBo-3.2.24"])
