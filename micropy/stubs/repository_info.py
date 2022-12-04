5afae4aad8b8f73f82177735275b5a80d2cdaacc
Braden Mars <bradenmars@bradenmars.me>
2022-12-04 04:41:30 -0600

    feat(stubs): validate RepoInfo source, add method for fetching contents.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- micropy/stubs/repository_info.py
+++ micropy/stubs/repository_info.py
@@ -1,9 +1,21 @@
 from __future__ import annotations
 
-from pydantic import BaseModel
+from datetime import timedelta
+from typing import Any
+
+import requests
+from cachier import cachier
+from pydantic import BaseModel, HttpUrl
 
 
 class RepositoryInfo(BaseModel):
     name: str
     display_name: str
-    source: str
+    source: HttpUrl
+
+    class Config:
+        frozen = True
+
+    @cachier(stale_after=timedelta(days=1), next_time=True)
+    def fetch_source(self) -> dict[str, Any]:
+        return requests.get(self.source).json()
