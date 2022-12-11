489a9b041082af18adf929471d7cc8f1f0bd39d5
Braden Mars <bradenmars@bradenmars.me>
2022-12-11 02:07:56 -0600

    feat(utils): add SupportsLessThan protocol to types util.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- micropy/utils/types.py
+++ micropy/utils/types.py
@@ -1,9 +1,14 @@
 """Type utilities and variables."""
-
 from os import PathLike
-from typing import Union
+from typing import Any, Union
 
-from typing_extensions import TypeAlias
+from typing_extensions import Protocol, TypeAlias, runtime_checkable
 
 # PathLike string or string type alias.
 PathStr: TypeAlias = Union[str, PathLike[str]]
+
+
+@runtime_checkable
+class SupportsLessThan(Protocol):
+    def __lt__(self, other: Any) -> bool:
+        ...
