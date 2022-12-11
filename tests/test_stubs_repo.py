2a9e24c5bf71e4ebf5d8e26e6457966e791a4ba2
Braden Mars <bradenmars@bradenmars.me>
2022-12-11 02:27:48 -0600

    test(stubs): expand stub repository unit tests.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- tests/test_stubs_repo.py
+++ tests/test_stubs_repo.py
@@ -1,28 +1,83 @@
 import pytest
+from micropy.stubs import RepositoryInfo, StubPackage, StubRepository, StubsManifest
 
 
-def test_repo_inits(test_repo):
-    assert len(test_repo.manifests) == 2
-    assert len(list(test_repo.packages)) == 3
+class ManifestStub(StubsManifest[StubPackage]):
+    def resolve_package_url(self, package: StubPackage) -> str:
+        return f"https://test-manifest/{package.name}"
+
+
+Test1Manifest = ManifestStub(
+    repository=RepositoryInfo(
+        name="Test", display_name="Test Display", source="https://test-manifest.com"
+    ),
+    packages=frozenset(
+        [
+            StubPackage(name="stub1-foo", version="1.0.0"),
+            StubPackage(name="stub1-foo", version="1.1.0"),
+            StubPackage(name="stub1-foo", version="2.0.0"),
+            StubPackage(name="stub2-bar", version="2.0.0"),
+        ]
+    ),
+)
+
+Test2Manifest = ManifestStub(
+    repository=RepositoryInfo(
+        name="Test2", display_name="Test Display2", source="https://test2-manifest.com"
+    ),
+    packages=frozenset(
+        [
+            StubPackage(name="stub3-thing", version="3.0.0"),
+            StubPackage(name="stub3-thing", version="3.1.0"),
+            StubPackage(name="stub4-device", version="4.0.0"),
+        ]
+    ),
+)
+
+
+@pytest.fixture
+def stub_repo():
+    repo = StubRepository(manifests=[Test1Manifest, Test2Manifest])
+    return repo
+
+
+def test_repo_inits(stub_repo):
+    assert len(stub_repo.manifests) == 2
+    assert len(list(stub_repo.packages)) == 7
 
 
 @pytest.mark.parametrize(
-    "query,expect_name",
+    "query,expect_name,include_versions",
     [
-        ("esp32", ["micropython-esp32-stubs-1.18.post1"]),
-        ("EsP32", ["micropython-esp32-stubs-1.18.post1"]),
-        ("8266", ["esp8266-micropython-1.15.0-abc123"]),
+        ("stub1", ["Test/stub1-foo-2.0.0"], False),
+        ("DEvICE", ["Test2/stub4-device-4.0.0"], False),
         (
-            "micropython",
+            "stub",
             [
-                "micropython-esp32-stubs-1.18.post1",
-                "micropython-7ff2cce0237268cd52164b77b6c2df6be6249a67ee285edc122960af869b8ed2",
-                "esp8266-micropython-1.15.0-abc123",
+                "Test/stub1-foo-2.0.0",
+                "Test/stub2-bar-2.0.0",
+                "Test2/stub3-thing-3.1.0",
+                "Test2/stub4-device-4.0.0",
             ],
+            False,
+        ),
+        (
+            "stub",
+            [
+                "Test/stub1-foo-1.0.0",
+                "Test/stub1-foo-1.1.0",
+                "Test/stub1-foo-2.0.0",
+                "Test/stub2-bar-2.0.0",
+                "Test2/stub3-thing-3.0.0",
+                "Test2/stub3-thing-3.1.0",
+                "Test2/stub4-device-4.0.0",
+            ],
+            True,
         ),
     ],
 )
-def test_repo_search(test_repo, query, expect_name):
-    results = test_repo.search(query)
-    names = [i.versioned_name for i in results]
+def test_repo_search(stub_repo, query, expect_name, include_versions):
+    results = stub_repo.search(query, include_versions=include_versions)
+    names = [i.absolute_versioned_name for i in results]
+    print(names)
     assert sorted(names) == sorted(expect_name)
